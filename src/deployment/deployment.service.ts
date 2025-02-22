5659caca294613eff2edd56727cd4ff3e3988e44
0xBiggy <0xBiggy@proton.me>
2025-02-22 16:12:51 +1100

    update start block

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -87,7 +87,7 @@ export class DeploymentService {
         harvestEventsBatchSize: 2000,
         harvestConcurrency: 10,
         multicallAddress: '0xcA11bde05977b3631167028862bE2a173976CA11',
-        startBlock: 18438182,  // todo
+        startBlock: 1453495,  // todo
         gasToken: {
           name: 'BERA',
           symbol: 'BERA',
